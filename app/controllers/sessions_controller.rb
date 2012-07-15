32fbfa1fcbdc3c7bcd29b6e442794e4581279bca
Kim D Barnes <dkmmbarnes@gmail.coM>
2012-07-15 14:12:28 -0600

    ch 9 savepoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,15 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:session][:email])
-    if (user && user.authenticate(params[:session][:password]))
+    if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
-      flash.now[:error] = "Invalid email/password combination"
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
-    
-  end
-  
+  end  
+      
   def destroy
     sign_out
     redirect_to root_path
